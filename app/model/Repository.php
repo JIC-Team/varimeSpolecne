63ff9475b7e22ad641a30b94fdf243860bd12eb6
PohyCZ <0ronon0@gmail.com>
2013-11-23 20:57:01 +0100

    added comments to Repository class

--- app/model/Repository.php
+++ app/model/Repository.php
@@ -2,6 +2,10 @@
 
 use Nette;
 
+/**
+* Provádí operace nad databázovou tabulkou.
+* @author David Pohan
+*/
 class Repository extends Nette\Object
 {
 	protected $connection;
@@ -11,17 +15,32 @@ class Repository extends Nette\Object
 		$this->connection = $connection;
 	}
 
+	/**
+     * Vrací objekt reprezentující databázovou tabulku.
+     * @return Nette\Database\Table\Selection
+     * @author David Pohan
+     */
 	protected function getTable()
 	{
 		preg_match('#(\w+)Repository$#', get_class($this), $m);
         return $this->connection->table(lcfirst($m[1]));
 	}
 
+	/**
+     * Vrací všechny řádky z tabulky.
+     * @return Nette\Database\Table\Selection
+     * @author David Pohan
+     */
 	public function findAll()
 	{
 		return $this->getTable();
 	}
 
+	/**
+     * Vrací řádky podle filtru, např. array('name' => 'John').
+     * @return Nette\Database\Table\Selection
+     * @author David Pohan
+     */
 	public function findBy(array $by)
 	{
 		return $this->getTable()->where($by);
