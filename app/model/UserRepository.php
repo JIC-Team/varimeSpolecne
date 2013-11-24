382506f98a3aaeb243e1d6b44517f87bd5415175
Simon <sapp2sapp@gmail.com>
2013-11-24 15:31:37 +0100

    Register Update

--- app/model/UserRepository.php
+++ app/model/UserRepository.php
@@ -1,35 +1,30 @@
 <?php
 
-/**
-* User class
-*/
+use Nette\Database\Connection;
+use Nette\Database\Table\ActiveRow;
+use Nette\Security\Identity;
+
 class UserRepository extends Repository
-{
-	/**
-	 * Creates new user
-	 * @param string $first_name
-	 * @param string $last_name
-	 * @param string $password      
-	 * @param string $email
-	 * @param string $gender
-	 * @return Nette\Database\Table\ActiveRow
-	 * @author Šimon Appelt
-	 */
-	public function register($data)
-	{
-		return $this->getTable()->insert(array(
-      'first_name' => $data->first_name,
-      'last_name' => $data->last_name,
-      'password' => Authenticator::calculateHash($data->password),
-      'email' => $data->email,
-			'date' => new \DateTime(),
-      'gender' => $data->gender,
-		));
-	}
+{    
+  public function findUser(array $by)
+  {
+      return $this->getTable()->where($by)->fetch();
+  }  
   
-  public function existUserEmail($email)
-	{
-		$result = $this->getTable()->where("email", $email)->count("*");
-    return $result;
-	}
+  public function registerUser($values)
+  {
+    return $this->getTable()->insert(array(
+      'first_name' => $values->first_name,
+      'last_name' => $values->last_name,
+      'password' => Authenticator::calculateHash($values->password),
+      'email' => $values->email,
+			'date' => new \DateTime(),
+      'gender' => $values->gender,
+  	));
+  }
+  
+  public function updateUser(ActiveRow $user, array $values)
+  {
+      $user->update($values);
+  }
 }
