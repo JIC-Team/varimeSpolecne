342251ac84e9f1f363955448fc798f95fe443f7f
PohyCZ <0ronon0@gmail.com>
2013-12-05 11:06:06 +0100

    added basic notification system

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -25,18 +25,9 @@ class HomepagePresenter extends BasePresenter
 
 	public function actionDefault()
 	{
-		$this->events = $this->context->eventRepository->find(array('user_id' => $this->getUser()->id));
+		$this->attendees = $this->context->attendeeRepository->findAll();
+		// $this->events = $this->context->eventRepository->find(array('user_id' => $this->getUser()->id));
 
-		$this->attendees = array();
-		foreach($this->context->attendeeRepository->findAll() as $attendee)
-		{
-			foreach($this->events as $event)
-			{
-				if($event->id == $attendee->event_id)
-					$this->attendees[] = $this->context->userRepository->find(array('id' => $attendee->user_id));
-			}
-		}
-
-		$this->template->notifications = $this->notifications;
+		$this->template->attendees = $this->attendees;
 	}
 }
